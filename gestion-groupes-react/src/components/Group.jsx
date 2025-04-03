a2ee5e990f79af3fbe32591cf06b4cd5af2fc51a
40TJG940 <julien.bonnet@ynov.com>
2025-04-03 12:49:17 +0200

    test

--- gestion-groupes-react/src/components/Group.jsx
+++ gestion-groupes-react/src/components/Group.jsx
@@ -41,26 +41,31 @@ const Group = ({ groupNumber, groupListMember, onDelete, onAddMember, memberCoun
         </button>
       </div>
 
-      {/* Affichage des membres du groupe en tableau 2x2 */}
-      <div className={classes.memberGrid}>
-        {groupListMember.map((member, index) => (
-          <div key={`${groupNumber}-member-${index}`} className={classes.memberCell}>
-            <span className={classes.memberName}>{member}</span>
-          </div>
-        ))}
+      <div className={classes.groupContent}>
+        {/* Affichage des membres du groupe en tableau 2x2 */}
+        <div className={classes.memberGrid}>
+          {groupListMember.map((member, index) => (
+            <div key={`${groupNumber}-member-${index}`} className={classes.memberCell}>
+              <span className={classes.memberName} title={member}>{member}</span>
+            </div>
+          ))}
+        </div>
       </div>
 
-      {/* Bouton pour ajouter un membre */}
-      <div className={classes.addMemberButton} onClick={() => setIsAdding(!isAdding)}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <line x1="12" y1="5" x2="12" y2="19"></line>
-          <line x1="5" y1="12" x2="19" y2="12"></line>
-        </svg>
-      </div>
+      {/* Zone des boutons d'action */}
+      <div className={classes.groupActions}>
+        {/* Nombre de membres avec indication visuelle si complet */}
+        <div className={`${classes.memberCount} ${isComplete ? classes.completeCount : ''}`}>
+          {memberCount}
+        </div>
 
-      {/* Nombre de membres avec indication visuelle si complet */}
-      <div className={`${classes.memberCount} ${isComplete ? classes.completeCount : ''}`}>
-        {memberCount}
+        {/* Bouton pour ajouter un membre */}
+        <div className={classes.addMemberButton} onClick={() => setIsAdding(!isAdding)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <line x1="12" y1="5" x2="12" y2="19"></line>
+            <line x1="5" y1="12" x2="19" y2="12"></line>
+          </svg>
+        </div>
       </div>
 
       {/* Formulaire pour ajouter un membre */}
